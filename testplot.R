cce63b1ca42dacfa1dab48a852a6810d6cacba3c
Péter Kalicz <kaliczp@gmail.com>
2023-07-13 18:22:29 +0200

    Background grid is added

--- testplot.R
+++ testplot.R
@@ -9,11 +9,15 @@ Elt.Hatter2.bxp$stats[,1:13] <- Elt.Hatter2.bxp$stats[,1:13] + 800
 pdf(width = 14)
 par(family = "serif", mar = c(3.1,4.1, 1.1, 4.1))
 bxp(Elt.Hatter1.bxp, ylim = c(-30,200), yaxs = "i",
+    outline = FALSE, border = FALSE,
+    ylab = "", axes = FALSE, type = "n")
+grid(nx = NA, ny = NULL, lty = 1)
+bxp(Elt.Hatter1.bxp, ylim = c(-30,200), yaxs = "i",
     boxfill = c(rep("white", 4), rep("lightblue",10)),
-    ylab = "Difference from background average (%)")
+    ylab = "Difference from background average (%)", add = TRUE)
 par(new = TRUE)
-bxp(Elt.Hatter2.bxp, ylim = c(-90,600), yaxs = "i", yaxt = "n")
-axis(1,13.5, tck = 1, lab = FALSE, col = "lightgray")
+bxp(Elt.Hatter2.bxp, ylim = c(-90,600), yaxs = "i", yaxt = "n", boxfill = "white")
+axis(1,13.5, tck = 1, lab = FALSE, lty = "dotted")
 axis(4, at = c(0, 150, 300, 450, 600))
 mtext(text = "Difference from background average (%)", side = 4, line = 3)
 dev.off()
